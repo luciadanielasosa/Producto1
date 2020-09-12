ca2567faf536bd76a9041a2be3ddd0dde2ce1b31
luciadanielasosa <a2019118793@estudiantes.upsa.edu.bo>
2020-09-12 12:05:15 -0400

    Creación de MVC

---
+++ ProductEnum/ProductEnum/Models/Product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProductEnum.Models
+{
+    public enum CategoryType
+    {
+        Electronicos=10,
+        Alimenticios=20,
+        Moda=30,
+        Belleza=40,
+        Automotriz=50,
+        Libros=60
+
+    }
+
+
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+
+        [Display(Name ="Nombre del producto")]
+        [Required(ErrorMessage ="Debe ingresar el nombre del producto")]
+        public string Name { get; set; }
+
+
+        [Display(Name = "Categoría del producto")]
+        [Required(ErrorMessage = "Debe ingresar la categoría del producto")]
+        public CategoryType Category { get; set; }
+    }
+}
